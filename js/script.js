7bd34158be322965a7d2393c1c858e4c363c8758
Glinko <forearnings78@gmail.com>
2023-07-13 08:54:43 +0300

    Firtst Commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+ "use strict";
+
+//Less = 006
+
+// let number = 4.6
+
+// console.log(-4/0);
+
+// const persone = "Alex";
+
+// const obj ={
+//   name : 'Alex',
+//   age: 34,
+//   isMarried: true,
+// };
+
+// console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'aplle.bmp',{
+//   name : 'Alex',
+//   age: 34,
+//   isMarried: true,
+// },[1,2,3,4,5,66] ];
+
+// console.log(arr);
+
+
+//Less = 007
+
+// alert("Hello");
+
+// const result = confirm('Are you sure you want to?');
+
+// console.log(result);
+
+// const answer = prompt('Are you 18th or mote ?');
+// console.log(answer);
+
+
+//let data = curl --location --request GET 'http://194.126.203.180:10158/v1_00/RoomStatus' --header 'API-TOKEN: 5ff5f689-1a11-48b8-ab88-645f236528da'

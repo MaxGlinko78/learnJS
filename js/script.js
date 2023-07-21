8764240a3eddf24bf2003bbe451e819c315c37d3
Glinko <forearnings78@gmail.com>
2023-07-21 11:15:36 +0300

    less 16

--- js/script.js
+++ js/script.js
@@ -39,27 +39,109 @@
 
 //let data = curl --location --request GET 'http://194.126.203.180:10158/v1_00/RoomStatus' --header 'API-TOKEN: 5ff5f689-1a11-48b8-ab88-645f236528da'
 
-const numberOfFilms = prompt('Сколько фильмов вы посмотрели ?');
+// const numberOfFilms = prompt('Сколько фильмов вы посмотрели ?');
+
+// lessons - 13
+
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies :{},
+//   actors:{},
+//   ganers:[],
+//   privat: false,
+// }
+
+// const a = prompt('Один из последних фильмов',''),
+//       b = prompt('На сколько оцените его ?',''),
+//       c = prompt('Один из последних фильмов',''),
+//       d = prompt('На сколько оцените его ?','');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// // personalMovieDB.movies = prompt('Один из последних фильмов');
+// // personalMovieDB.movies = prompt('На сколько оцените его ?');
+// //
+//  console.log(personalMovieDB);
+
+// lessons - 14
+
+
+// let num = 50;
+
+// while (num < 55) {
+//   console.log(num);
+//   num++;
+// }
+
+
+// do {
+//   console.log(num);
+//   num++; }
+//   while (num <= 55);
+
+
+// for(let i =1; i < 10; i++) {
+// if(i === 6){
+//   //break;
+//   continue;
+// }
+
+
+//   console.log(i);
+
+// };
+
+// lesson 15
+// const numberOfFilms = prompt('Сколько фильмов вы посмотрели ?');
+
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies :{},
+//   actors:{},
+//   ganers:[],
+//   privat: false,
+// }
 
 
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies :{},
-  actors:{},
-  ganers:[],
-  privat: false,
+
+// for(let i= 0; i<2; i++){
+//    const a = prompt('Один из последних фильмов',''),
+//        b = prompt('На сколько оцените его ?','');
+//        if (a != null && b != null && a !='' && b !='' && a.length < 50){
+//           personalMovieDB.movies[a] = b;
+//           console.log('Done');
+//        } else {
+//         console.log('Error');
+//         i--;
+//       }
+
+// }
+
+
+// if (personalMovieDB.count < 10 ){
+//   console.log('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+//     console.log('Вы классический зритель');
+// }
+//  else if (personalMovieDB.count > 30){
+//     console.log('Вы киноман');
+// } else{
+//    console.log('Произошла ошибка');
+// }
+
+// console.log(personalMovieDB)
+
+
+// lesson 16 
+let num = 20;
+
+function showFirstMessage(text) {
+console.log(text);
+let num = 10;
+  
 }
 
-const a = prompt('Один из последних фильмов',''),
-      b = prompt('На сколько оцените его ?',''),
-      c = prompt('Один из последних фильмов',''),
-      d = prompt('На сколько оцените его ?','');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-// personalMovieDB.movies = prompt('Один из последних фильмов');
-// personalMovieDB.movies = prompt('На сколько оцените его ?');
-// 
- console.log(personalMovieDB);
+showFirstMessage("First Message");
+console.log(num);
